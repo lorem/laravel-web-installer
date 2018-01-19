580a21ce7912f93bcbd252fd76d4be171ecbd35a
Paradigm <rob@nulldev.uk>
2018-01-19 16:45:06 +0000

    Added error handling

--- src/Helpers/DatabaseManager.php
+++ src/Helpers/DatabaseManager.php
@@ -9,19 +9,36 @@ use Illuminate\Support\Facades\DB;
 class DatabaseManager
 {
     /**
+     * @var array
+     */
+    private $errors = [];
+
+    /**
      * Migrate and seed the database.
      */
     public function migrateAndSeed()
     {
+        $this->testDatabaseConnection();
+        $this->migrate();
+        $this->seed();
+        if (count($this->errors) > 0)
+        {
+            return view('installer::environment')->with(['errors' => $this->errors]);
+        }
+    }
+
+    /**
+     * Test the database connection.
+     */
+    protected function testDatabaseConnection()
+    {
         try {
             DB::connection()->getPdo();
         } catch (\Exception $e) {
-            die('Could not connect to the database.  Please check your configuration.');
+            $this->addError($e);
         }
-
-        $this->migrate();
-        $this->seed();
     }
+
 
     /**
      * Migrate the tables.
@@ -31,7 +48,7 @@ class DatabaseManager
         try {
             Artisan::call('migrate', ['--force' => true]);
         } catch (Exception $e) {
-            return $e;
+            $this->addError($e);
         }
     }
 
@@ -43,7 +60,17 @@ class DatabaseManager
         try {
             Artisan::call('db:seed');
         } catch (Exception $e) {
-            return $e;
+            $this->addError($e);
         }
+    }
+
+    /**
+     * Add an error.
+     *
+     * @param $error
+     */
+    protected function addError($error)
+    {
+        $this->errors[] = $error;
     }
 }
